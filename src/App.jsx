6dadbd2f54ca7928e589d7f11a00b1364c4c696f
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-28 15:44:56 +0500

    Simple styling done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import randomNumber from "./helpers/randomNumber.js";
+import PokemonCard from "./components/PokemonCard/PokemonCard.jsx";
+function App() {
+  const pokemonCards = [];
+  for (let i = 0; i < 10; i++) {
+    const pokemonNumber = randomNumber(1, 1025);
+    pokemonCards.push(
+      <PokemonCard key={pokemonNumber} pokemonNumber={pokemonNumber} />
+    );
+  }
+  return <div className="grid">{pokemonCards}</div>;
+}
+
+export default App;
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -6,7 +6,13 @@ export default function PokemonCard({ pokemonNumber }) {
   const [pokemonData, setPokemonData] = useState();
   const [speciesData, setSpeciesData] = useState();
 
-  const color = speciesData ? speciesData.color.name : "white";
+  const habitat =
+    speciesData && speciesData.habitat ? speciesData.habitat.name : "grassland";
+  const flavorText =
+    speciesData && speciesData.flavor_text_entries
+      ? speciesData.flavor_text_entries[0].flavor_text
+      : "";
+
   const pokemonName = pokemonData ? capitalize(pokemonData.name) : "Loading...";
   const imageUrl = pokemonData
     ? pokemonData.sprites.other["official-artwork"].front_default
@@ -42,8 +48,16 @@ export default function PokemonCard({ pokemonNumber }) {
     }
   }, [pokemonData]);
   return (
-    <div className="container" style={{ backgroundImage: `linear-gradient(135deg, white, ${color})` }}>
-      <img src={imageUrl} alt={``} />
+    <div className="container">
+      <img
+        src={imageUrl}
+        alt={``}
+        className="pokemon_img"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url("../../../public/habitats/${habitat}.png")`,
+        }}
+      />
       <h1>{pokemonName}</h1>
     </div>
   );
