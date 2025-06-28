3221ee51481e3cf54e11fc9899d10232393a654d
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-28 15:54:22 +0500

    Improved on hover of cards to be less abnoxious.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import randomNumber from "./helpers/randomNumber.js";
 import PokemonCard from "./components/PokemonCard/PokemonCard.jsx";
 function App() {
   const pokemonCards = [];
+  const pokemonIds = {};
   for (let i = 0; i < 10; i++) {
     const pokemonNumber = randomNumber(1, 1025);
-    pokemonCards.push(
-      <PokemonCard key={pokemonNumber} pokemonNumber={pokemonNumber} />
-    );
+    if (pokemonIds[pokemonNumber] !== 1) {
+      pokemonCards.push(
+        <PokemonCard key={pokemonNumber} pokemonNumber={pokemonNumber} />
+      );
+      pokemonIds[pokemonNumber] = 1;
+    }
   }
   return <div className="grid">{pokemonCards}</div>;
 }
